3d4a862f662249caec9cd71d29cca36770a9c6f3
bekoinettien <bekoinettien@gmail.com>
2024-04-07 03:03:03 +0000

    mise de la page propos

--- formation/urls.py
+++ formation/urls.py
@@ -4,5 +4,6 @@ from. import views
 
 urlpatterns = [
     path('', views.home,name='home'),
+    path('propos', views.propos,name='propos'),
      
 ]
--- formation/views.py
+++ formation/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 def home(resquest):
     return render(resquest,'home.html')
 
+def propos(resquest):
+    return render(resquest,'propos.html')
+
 # Create your views here.
